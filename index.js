bff2435a5c45f4875df6833289f85d9acd810111
Adrián <adrianglezm97@gmail.com>
2023-03-27 10:00:25 +0100

    First commit:
    -USBAT ver tablero
    -USBAT mover plataforma
    -USBAT ver pelota
    -USBAT mover pelota
    -USBAT pelota chocar pared
    -USBAT ver plataforma
    -USBAT ver las paredes
    -USBAT chocar plataforma

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ let paddle = document.querySelector("#paddle");
 let ballTop = 450;
 let direction = 1;
 
-const moveBallY = function() {
+const moveBallY = function () {
     if (ballTop < 5 || ballTop > 890) { direction *= -1; };
     ballTop += 10 * direction;
     ball.style.top = ballTop + "px";
+    console.log(ballTop)
 }
 
 let timerId;
-let moving  = false;
+let moving = false;
 
-ball.addEventListener("click", function(e) {
+ball.addEventListener("click", function (e) {
     if (!moving) {
         timerId = setInterval(moveBallY, 30);
     } else {
@@ -25,7 +26,7 @@ ball.addEventListener("click", function(e) {
 let ballLeft = 400;
 let direction2 = 1;
 
-const moveBallX = function() {
+const moveBallX = function () {
     if (ballLeft < 5 || ballLeft > 800) { direction2 *= -1; };
     ballLeft += 10 * direction2;
     ball.style.left = ballLeft + "px";
@@ -34,7 +35,7 @@ const moveBallX = function() {
 let timerId2;
 let moving2 = false;
 
-ball.addEventListener("click", function(e) {
+ball.addEventListener("click", function (e) {
     if (!moving2) {
         timerId = setInterval(moveBallX, 30);
     } else {
@@ -61,3 +62,35 @@ window.addEventListener('keydown', function (event) {
             break
     }
 })
+
+/*
+let paddleTop = paddle.style.top;
+let paddleTopBottom = paddleTop + paddle.style.height;
+let paddleLeftRight = paddleLeft + paddle.style.width;
+*/
+
+let paddleTop = 900;
+let paddleTopBottom = paddleTop + 15;
+let paddleLeftRight = paddleLeft + 175;
+
+let ballTopBottom = ballTop + 50;
+let ballLeftRight = ballLeft + 50;
+
+const collisionPaddleBall = function () {
+    if (paddleLeft < ballLeftRight &&
+        paddleTop < ballTopBottom &&
+        paddleLeftRight > ballLeft &&
+        paddleTopBottom > ballTop) {
+        direction *= -1;
+        direction2 *= -1;
+    }
+}
+console.log(paddleLeft)
+console.log(ballLeft)
+console.log(ballTop)
+console.log(paddleTopBottom)
+console.log()
+console.log()
+console.log()
+console.log(collisionPaddleBall());
+collisionPaddleBall()
